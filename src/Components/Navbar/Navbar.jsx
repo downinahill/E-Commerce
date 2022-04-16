77ab94086742785ec97b8fdb7138ed1fb6fe1b66
downinahill <brian.hill.0023@gmail.com>
2022-04-16 15:15:57 -0400

    made products component

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
+
---
+++ src/Data.js
@@ -0,0 +1,31 @@
+export const projects = [
+    {
+      title: "Sharing is Scary",
+      subtitle: "MERN Stack Art Sharing App",
+      description:
+        "This app allows people to share their artwork with others.",
+      image: "Screen Shot 2022-02-06 at 4.24.15 PM.png",
+      link: "https://bestartshare.herokuapp.com/",
+    },
+    {
+      title: "Accompli",
+      subtitle: "MERN Stack Productivity App",
+      description: "",
+      image: "Screen Shot 2022-02-05 at 12.51.04 PM.png",
+      link: "https://accompli-frontend-app.herokuapp.com/",
+    },
+    {
+      title: "",
+      subtitle: "",
+      description: "",
+      image: "",
+      link: "",
+    },
+    {
+      title: "",
+      subtitle: "",
+      description: "",
+      image: "",
+      link: "",
+    },
+  ];
